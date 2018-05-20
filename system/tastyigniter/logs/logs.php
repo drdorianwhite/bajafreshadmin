df54227039f0edab02c89326ec39824663ae98bc
Ubuntu <ubuntu@ip-172-31-22-11.us-west-2.compute.internal>
2018-05-20 02:34:38 +0000

    added tastyigniter files

---
+++ system/tastyigniter/logs/logs.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-05-20 01:03:21 --> Query error: Column 'user_agent' cannot be null - Invalid query: INSERT INTO `4gscfoejh_customers_online` (`ip_address`, `customer_id`, `access_type`, `browser`, `country_code`, `request_uri`, `referrer_uri`, `date_added`, `user_agent`) VALUES ('109.74.204.21', '0', '', '', 'GB', '', '', '2018-05-20 01:03:21', NULL)
